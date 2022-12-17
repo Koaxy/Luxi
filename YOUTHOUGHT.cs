fffd14cc99a64d5614b721bfe734ce5fe093cd70
Koaxy <92256387+Koaxy@users.noreply.github.com>
2022-12-17 08:40:29 +0000

    Create YOUTHOUGHT.cs

---
+++ YOUTHOUGHT.cs
@@ -0,0 +1,16 @@
+public class youthought()
+{
+public void LuxiSource()
+{
+   Console.WriteLine("Luxi source is so sex");
+}
+}
+
+
+
+private static string Main(string Args)
+{
+youthought.LuxiSource();
+}
+
+
